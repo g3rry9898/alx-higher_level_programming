e313c393905c9f38320fed26a67544f043a84019
g3rry9898 <hydrogenbond36@gmail.com>
2024-02-02 22:44:12 -0800

    m

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def magic_string():
-    return "BestSchool" *
-(magic_string.counter := getattr(magic_string, "counter", 0) +1)
+    return "BestSchool" *(magic_string.counter := getattr(magic_string, "counter", 0) +1)
