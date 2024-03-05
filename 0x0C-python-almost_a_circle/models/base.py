ddad0b549d4159df8bcfcb7c8b0f71273d0ff4b8
g3rry9898 <hydrogenbond36@gmail.com>
2024-03-05 06:40:35 -0800

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,22 @@
 class Base:
+      """
+          Base class for other classes.
+
+              Attributes:
+                      id (int): Unique identifier for instances. If not provided, it is auto-assigned.
+                          """
     __nb_objects = 0
     
     def __init__(self, id=None):
+         """
+                 Class constructor.
+
+                         Args:
+                                     id (int, optional): If provided, assigns the public instance attribute "id" with this value.
+                                                                     Otherwise, increments __nb_objects and assigns the new value to "id".
+                                                                             """
         if id is not None:
             self.id = id
         else:
              Base.__nb_objects += 1
-             self.id =  Base.__nb_objects
+                 self.id =  Base.__nb_objects
