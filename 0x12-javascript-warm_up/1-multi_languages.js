e2abd9c1b7d47df45e9adb4496f56b5f89dd50f9
g3rry9898 <hydrogenbond36@gmail.com>
2024-05-08 12:50:01 -0700

    java

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Print the first line
+const firstLine = "C is fun";
+console.log(firstLine);
+
+// Print the second line
+const secondLine = "Python is cool";
+console.log(secondLine);
+
+// Print the third line
+const thirdLine = "JavaScript is amazing";
+console.log(thirdLine);
+
