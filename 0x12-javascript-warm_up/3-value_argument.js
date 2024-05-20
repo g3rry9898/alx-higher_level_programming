ecef73dbe9b9215a3b7cf3c9fa7868b494a37913
g3rry9898 <hydrogenbond36@gmail.com>
2024-05-20 07:32:45 -0700

    warm up js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+const args = process.argv;
 
-if (process.argv.length <= 2) {
+if (args[2] === undefined) {
     console.log("No argument");
 } else {
-    console.log(process.argv[2]);
+    console.log(arg);
 }
 
