d16d9e2a0cf0839089c4bbda8afb42011d2f4abb
g3rry9898 <hydrogenbond36@gmail.com>
2024-08-29 06:34:39 -0700

    jkl

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,12 +4,8 @@ function add(a, b) {
     return a + b;
 }
 
-const num1 = parseInt(process.argv[2], 10);
-const num2 = parseInt(process.argv[3], 10);
+const firstArg = parseInt(process.argv[2], 10);
+const secondArg = parseInt(process.argv[3], 10);
 
-if (!isNaN(num1) && !isNaN(num2)) {
-    console.log("Sum:", add(num1, num2));
-} else {
-    console.log("Invalid input. Please provide two valid integers.");
-}
+console.log(add(firstArg, secondArg));
 
